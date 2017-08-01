28f37026d394c0268454638b26279b7aecb95635
sternj20 <jessesternmusic@gmail.com>
2017-08-01 13:18:27 -0700

    intial server setup and routing. built home page in HTML

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+
+require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
+
+app.listen(PORT, function(){
+	console.log("App listening on PORT " + PORT);
+});
+
