21bef66b692e1d6aeff00330e39b0da278b1643b
sternj20 <jessesternmusic@gmail.com>
2017-08-03 19:56:23 -0700

    fixed some bugs in logic that shows best match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,35 +15,33 @@ module.exports = function(app){
 		//array that will hold the score difference between the friend being added and each friend in storage
 		var differenceAmtArray = [];
 		//keeps track of difference amount per person
-		var totalDifferenceAmt;
+		var totalDifferenceAmt = 0;
 		var differenceAmt;
 		var bestMatch;
 		// iterate through the friends array
 		for(var i = 0; i < friends.length; i++){
-				for(var j = 0; j < 10; j++){
+			for(var j = 0; j < 10; j++){
 				//for each friend in array, subtract new friends scores from their scores and push to a new array
-				differenceAmt = newFriend.scores[j] - friends[i].scores[j];
+				differenceAmt = Math.abs(newFriend.scores[j] - friends[i].scores[j]);
 				// if the difference amount is a negative number
-				if(differenceAmt < 0){
-					//convert to positive
-					differenceAmt = Math.abs(differenceAmt);
-				}
-				totalDifferenceAmt = 0;
 				//add each scores difference to total
 				totalDifferenceAmt += differenceAmt;
+				console.log(totalDifferenceAmt);
 			}
 			//push each persons difference amount to their specific object in friends array
-			friends[i].totalDiff += differenceAmt;
+			friends[i].totalDiff = totalDifferenceAmt;
+			totalDifferenceAmt = 0;
 		}
 		friends.push(newFriend);
 		//sort the friends array by who has the lowest difference amount in score with the newest friend added
 		friends.sort(function(a,b){
 			return a.totalDiff - b.totalDiff;
 		});
-		//return the closest match
-		// console.log(friends[0]);
-		bestMatch = friends[0];
-		// console.log(bestMatch)
+		if(friends[0] !== newFriend){
+			bestMatch = friends[0];
+		} else {
+			bestMatch = friends[1];
+		}
 		res.json(bestMatch);
 	});
 };
